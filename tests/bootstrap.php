e35faada1bc29509155fe66b99e7c957cd3009d1
Ajay DSouza <me@ajaydsouza.com>
2021-03-06 22:48:27 +0000

    Updated bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,32 @@
 <?php
-
-$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-$_SERVER['SERVER_NAME']     = '';
-$PHP_SELF                   = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Contextual_Related_Posts
+ */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../top-10-fast-tracker.php';
+	require dirname( dirname( __FILE__ ) ) . '/top-10-fast-tracker.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
 
 activate_plugin( 'top-10-fast-tracker/top-10-fast-tracker.php' );
 
-echo "Installing Top 10 Fast Tracker...\n";
+echo "Top 10 Fast Tracker...\n";
+
+global $current_user;
 
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,10 +1,20 @@
 <?php
+/**
+ * Class SampleTest
+ *
+ * @package Top_Ten_Fast_Tracker
+ */
 
+/**
+ * Sample test case.
+ */
 class SampleTest extends WP_UnitTestCase {
 
-	function testSample() {
-		// replace this with some actual testing code
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
 }
-
